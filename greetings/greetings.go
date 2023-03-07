de5f0df162740b9e545635210de30c9d4d262a0d
Sebastian Borza <sebito91@gmail.com>
2023-03-07 11:45:52 -0600

    feat(greetings): add random greetings

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person.
@@ -12,5 +14,22 @@ func Hello(name string) (string, error) {
 	}
 
 	// Return a greeting that embeds the name in a message
-	return fmt.Sprintf("Hi, %v. Welcome!", name), nil
+	return fmt.Sprintf(randomFormat(), name), nil
+}
+
+// init sets initial values for variables used in the function
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	formats := []string{
+		"Great to see you %v!",
+		"Hi, %v. Welcome, yo!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
